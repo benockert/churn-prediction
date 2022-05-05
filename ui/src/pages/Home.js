904b16cfe78899978bf901841d7e931e9c25605b
benockert <55101856+benockert@users.noreply.github.com>
2022-05-05 08:46:08 -0400

    Added source buttons on home page

--- ui/src/pages/Home.js
+++ ui/src/pages/Home.js
@@ -2,6 +2,9 @@ import { useEffect, useState } from 'react';
 import { DefaultLayout } from "../layouts/DefaultLayout";
 import { api_get } from '../utils/api.js';
 import { Loading } from '../components/Loading';
+import { BsGithub } from 'react-icons/bs';
+import Button from 'react-bootstrap/Button';
+import './Home.css';
 
 export const Home = () => {
     const [title, setTitle] = useState();
@@ -24,6 +27,10 @@ export const Home = () => {
         setLoading(!title || !names);
     }, [title, names])
 
+    const openPage = (link) => {
+        window.open(link);
+    }
+
     return (
         <DefaultLayout>
             {
@@ -31,8 +38,16 @@ export const Home = () => {
                     <Loading />
                     :
                     <>
-                        <h1>{title}</h1>
+                        <h1 className="title-text">{title}</h1>
                         <h3>{names}</h3>
+                        <span>
+                            <Button onClick={() => openPage("https://github.com/benockert/churn-prediction/tree/main/ui")} className="submit-button home-button" variant="primary" type="submit">
+                                Source on <BsGithub />
+                            </Button>
+                            <Button onClick={() => openPage("https://fina-4390.s3.amazonaws.com/Final+Project-+ML+full.pdf")} className="submit-button home-button" variant="primary" type="submit">
+                                Full Presentation
+                            </Button>
+                        </span>
                     </>
             }
         </DefaultLayout>
